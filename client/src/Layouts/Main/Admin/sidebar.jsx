404623a46a36b34a58a37fc8209971ecca8677ae
Ace Hood <101823587+acehood0126@users.noreply.github.com>
2022-06-21 03:50:36 +0900

    Dashboard

--- client/src/Layouts/Main/Admin/sidebar.jsx
+++ client/src/Layouts/Main/Admin/sidebar.jsx
@@ -1,12 +1,14 @@
 import { MdOutlineDashboard, MdOutlineBusinessCenter } from "react-icons/md";
 import { FiBox, FiUsers } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import logo from "../../../Assets/Images/logo.png";
 
 const Sidebar = () => {
+  const location = useLocation();
+
   return (
-    <div className="w-full h-full border-r-[1px] border-darkblue/5 font-bold bg-transparent">
+    <div className="w-full h-full border-r-[1px] border-darkblue/0 font-bold bg-transparent">
       <Link
         to="/"
         className="w-full text-[12px] mt-[30px] ml-[20px] flex items-center"
@@ -17,28 +19,44 @@ const Sidebar = () => {
       <div className="w-full h-full text-[12px] mt-[30px]">
         <Link
           to="/admin/dashboard"
-          className="flex items-center my-[20px] pl-[40px] w-full border-l-[3px] border-transparent hover:border-darkblue transition ease-linear duration-300 py-[5px]"
+          className={`flex items-center my-[20px] pl-[40px] w-full border-l-[3px] ${
+            location.pathname === "/admin/dashboard"
+              ? "border-darkblue"
+              : "border-transparent"
+          } hover:border-darkblue transition ease-linear duration-300 py-[5px]`}
         >
           <MdOutlineDashboard className="w-[18px] h-[18px] mr-[15px]" />
           <p>Dashboard</p>
         </Link>
         <Link
           to="/admin/companies"
-          className="flex items-center my-[20px] pl-[40px] w-full border-l-[3px] border-transparent hover:border-darkblue transition ease-linear duration-300 py-[5px]"
+          className={`flex items-center my-[20px] pl-[40px] w-full border-l-[3px] ${
+            location.pathname === "/admin/companies"
+              ? "border-darkblue"
+              : "border-transparent"
+          } hover:border-darkblue transition ease-linear duration-300 py-[5px]`}
         >
           <MdOutlineBusinessCenter className="w-[18px] h-[18px] mr-[15px]" />
           <p>Companies</p>
         </Link>
         <Link
           to="/admin/products"
-          className="flex items-center my-[20px] pl-[40px] w-full border-l-[3px] border-transparent hover:border-darkblue transition ease-linear duration-300 py-[5px]"
+          className={`flex items-center my-[20px] pl-[40px] w-full border-l-[3px] ${
+            location.pathname === "/admin/products"
+              ? "border-darkblue"
+              : "border-transparent"
+          } hover:border-darkblue transition ease-linear duration-300 py-[5px]`}
         >
           <FiBox className="w-[18px] h-[18px] mr-[15px]" />
           <p>Products</p>
         </Link>
         <Link
           to="/admin/users"
-          className="flex items-center my-[20px] pl-[40px] w-full border-l-[3px] border-transparent hover:border-darkblue transition ease-linear duration-300 py-[5px]"
+          className={`flex items-center my-[20px] pl-[40px] w-full border-l-[3px] ${
+            location.pathname === "/admin/users"
+              ? "border-darkblue"
+              : "border-transparent"
+          } hover:border-darkblue transition ease-linear duration-300 py-[5px]`}
         >
           <FiUsers className="w-[18px] h-[18px] mr-[15px]" />
           <p>Users</p>
--- client/src/Pages/Admin/Dashboard/index.jsx
+++ client/src/Pages/Admin/Dashboard/index.jsx
@@ -1,5 +1,13 @@
+import avatar from "../../../Assets/Images/avatars/avatar1.jpg";
+
 const AdminDashboard = () => {
-  return <div className="w-full p-[30px]">Dashboard Page</div>;
+  return (
+    <div className="w-full p-[30px]">
+      <div className="w-full h-[150px] bg-specialwhite drop-shadow-threeDboxOuter rounded-l-full rounded-r-2xl overflow-hidden">
+        <img src={avatar} className="w-[150px] h-[150px] rounded-full" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
