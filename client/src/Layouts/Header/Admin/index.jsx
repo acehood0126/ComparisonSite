18f47b4c1704d39865c29e27b8f1e20edd068aa3
Ace Hood <101823587+acehood0126@users.noreply.github.com>
2022-06-21 01:37:56 +0900

    favicon

--- client/src/Layouts/Header/Admin/index.jsx
+++ client/src/Layouts/Header/Admin/index.jsx
@@ -3,11 +3,7 @@ import avatar from "../../../Assets/Images/avatars/avatar1.jpg";
 const AdminHeader = () => {
   return (
     <div className="w-full h-[60px] px-[20px] bg-transparent flex justify-end items-center">
-      <img
-        src={avatar}
-        alt=""
-        className="w-[40px] h-[40px] rounded-full border-[2px] border-white"
-      />
+      <img src={avatar} alt="" className="w-[30px] h-[30px] rounded-full" />
     </div>
   );
 };
